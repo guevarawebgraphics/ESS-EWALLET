b0062c091173016b51bdd32a730fbb2334990f54
Man <mmsilvano@circuit-solutions.net>
2019-12-18 14:44:03 +0800

    list wallet accounts

---
+++ resources/js/components/ListWalletAccounts/WalletAccountProfile.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="wallet-accouunt-profile">
+        <div class="box ptb--100">
+            <!-- Card -->
+                <div class="card shadow-custom">
+                    <!-- Card body -->
+                        <div class="card-body">
+                            <!-- Form Group Row -->
+                            <div class="form-group row">
+                                <!-- Col -->
+                                <div class="col-md-12">
+                                    <i class="ti-arrow-left" @click="back"></i>
+                                    <h4 class="header-title mt3"> </h4>
+                                    <div class="form-group row">
+                                        <div class="col md-6">
+                                            <a class="btn btn-primary btn-md text-white">Available Services</a>
+                                            <a class="btn btn-primary btn-md text-white">List of Merchants</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- ./ Col -->
+                            </div>
+                            <!-- ./ Form Group Row -->
+                        </div>
+                    <!-- ./ Card body -->
+                </div>
+            <!-- ./ Card -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{}
+    },
+    methods: {
+        back(){
+            this.$router.push('/listwalletaccounts')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
